d16b867dba07c449a71c1adc84a460696a17ee0c
Deepanshi30 <deepanshimarch30@gmail.com>
2022-05-19 22:25:48 +0530

    Add project files.

---
+++ Controllers/StaffingController.cs
@@ -0,0 +1,32 @@
+﻿
+using System;  
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Web;  
+using System.Web.Mvc;  
+using StaffingProject2.Models;  
+  
+namespace StaffingProject2.Controllers
+{
+    public class StaffingController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Registration obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                StaffingEntities1 db = new StaffingEntities1();
+                db.Registrations.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
